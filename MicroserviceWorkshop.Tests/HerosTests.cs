5f58218dc8dd9403501e7a115d36d44ea65430af
Henry Keen <henry.keen@buildcircle.co.uk>
2018-08-19 16:53:30 +0100

    test for successful status code

--- MicroserviceWorkshop.Tests/HerosTests.cs
+++ MicroserviceWorkshop.Tests/HerosTests.cs
@@ -1,14 +1,28 @@
 using System;
 using Xunit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using MicroserviceWorkshop;
+using System.Threading.Tasks;
+using FluentAssertions;
+using System.Net;
 
 namespace MicroservicesWorkshop.Tests
 {
     public class HerosTests
     {
         [Fact]
-        public void CanGetHeros()
+        public async Task CanGetHeros()
         {
-            
+            var startup = new WebHostBuilder()
+                            .UseStartup<Startup>();
+
+            var testServer = new TestServer(startup);
+            var client = testServer.CreateClient();
+
+            var response = await client.GetAsync("heros");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
     }
 }
