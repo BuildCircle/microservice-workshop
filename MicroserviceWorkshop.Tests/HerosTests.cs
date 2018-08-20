08f358718a604be77771066c15629c4d974b0939
Henry Keen <henry.keen@buildcircle.co.uk>
2018-08-20 23:00:46 +0100

    empty everything

--- MicroserviceWorkshop.Tests/HerosTests.cs
+++ MicroserviceWorkshop.Tests/HerosTests.cs
@@ -17,62 +17,7 @@ namespace MicroserviceWorkshop.Tests
         [Fact]
         public async Task CanGetHeros()
         {
-            var charactersProvider = new FakeCharactersProvider();
-
-            var startup = new WebHostBuilder()
-                            .UseStartup<Startup>()
-                            .ConfigureServices(x => 
-                            {
-                                x.AddSingleton<ICharactersProvider>(charactersProvider);
-                            });
-            var testServer = new TestServer(startup);
-            var client = testServer.CreateClient();
-
-            charactersProvider.FakeResponse(new CharactersResponse
-            {
-                Items = new []
-                {
-                    new CharacterResponse
-                    {
-                        Name = "Batman",
-                        Score = 8.3,
-                        Type = "hero"
-                    },
-                    new CharacterResponse
-                    {
-                        Name = "Joker",
-                        Score = 8.2,
-                        Type = "villain"
-                    }
-                }
-            });
-
-            var response = await client.GetAsync("heros");
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-
-            var responseJson = await response.Content.ReadAsStringAsync();
-            var responseObject = JsonConvert.DeserializeObject<JObject>(responseJson);
-
-            var items = responseObject.Value<JArray>("items");
-            items.Count.Should().Be(1);
-
-            items[0].Value<string>("name").Should().Be("Batman");
-            items[0].Value<decimal>("score").Should().Be(8.3m);
-        }
-    }
-
-    public class FakeCharactersProvider : ICharactersProvider
-    {
-        CharactersResponse _response;
-        
-        public void FakeResponse(CharactersResponse response)
-        {
-            _response = response;
-        }
-
-        public Task<CharactersResponse> GetCharacters()
-        {
-            return Task.FromResult(_response);
+            true.Should().BeTrue();
         }
     }
 }
